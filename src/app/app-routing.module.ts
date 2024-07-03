0016942c9f17d2c1cea16901eb48941394a81da4
IgorMontezuma20 <igorminranda17@gmail.com>
2024-07-02 23:48:42 -0300

    feat: Criação dos módulos base da aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'company', loadChildren: () => import('./modules/company/company.module').then(m => m.CompanyModule) }, { path: 'client', loadChildren: () => import('./modules/client/client.module').then(m => m.ClientModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
