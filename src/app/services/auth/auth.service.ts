36fee39db412f527c473548c2f69a36bb5a59dbf
IgorMontezuma20 <igorminranda17@gmail.com>
2024-07-08 20:45:06 -0300

    feat: Create register methods

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const BASIC_URL = 'http://localhost:8080/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  registerClient(signupRequestDTO: any): Observable<any> {
+    return this.http.post(BASIC_URL + 'client/sign-up', signupRequestDTO);
+  }
+  registerCompany(signupRequestDTO: any): Observable<any> {
+    return this.http.post(BASIC_URL + 'company/sign-up', signupRequestDTO);
+  }
 }
